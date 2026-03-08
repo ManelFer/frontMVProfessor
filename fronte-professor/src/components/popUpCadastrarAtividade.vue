5c6396d472139e97bef467068c791bc668a33e40
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2026-03-08 13:01:52 -0300

    update

---
+++ fronte-professor/src/components/popUpCadastrarAtividade.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+
+import { X } from "lucide-vue-next"
+import { ref } from "vue"
+
+const emit = defineEmits(["close", "save"])
+
+const nome = ref("")
+const descricao = ref("")
+const link = ref("")
+
+
+function salvar() {
+
+    emit("save", {
+        nome: nome.value,
+        descricao: descricao.value,
+        link: link.value
+    })
+
+    emit("close")
+
+}
+
+</script>
+
+<template>
+
+    <div class="overlay">
+
+        <div class="modal">
+
+            <div class="modal-header">
+
+                <h2>Nova Atividade</h2>
+
+                <button class="close" @click="$emit('close')">
+                    <X class="icon" />
+                </button>
+
+            </div>
+
+            <div class="form">
+
+                <div class="form-group">
+                    <label>Nome da atividade</label>
+                    <input v-model="nome" placeholder="Ex: Lista de exercícios 1" />
+                </div>
+
+                <div class="form-group">
+                    <label>Descrição rápida</label>
+                    <textarea v-model="descricao" placeholder="Descreva a atividade" />
+                </div>
+
+                <div class="form-group">
+                    <label>Link da atividade</label>
+                    <input v-model="link" placeholder="https://..." />
+                </div>
+
+            </div>
+
+            <div class="actions">
+
+                <button class="btn-cancel" @click="$emit('close')">
+                    Cancelar
+                </button>
+
+                <button class="btn-save" @click="salvar">
+                    Criar atividade
+                </button>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+<style scoped src="../style/popUpCadastrarAtividade.css"></style>
--- fronte-professor/src/pages/AdicionarAtividade.vue
+++ fronte-professor/src/pages/AdicionarAtividade.vue
@@ -1,12 +1,54 @@
 <script setup lang="ts">
+import { Plus, Search } from 'lucide-vue-next';
 import SideBar from '../components/sideBar.vue';
+import popUpCadastrarAtividade from '../components/popUpCadastrarAtividade.vue';
+import { ref } from 'vue';
+const abrirModal = ref(false)
 
 
 </script>
 <template>
     <div class="layout">
-        <SideBar/>
-        <h1>teste</h1>
+        <SideBar />
+        <div class="main">
+            <div class="content">
+                <!-- Header -->
+                <div class="page-header">
+                    <div>
+                        <h1> Atividade </h1>
+                        <p> Crie e gerencie atividades para seus alunos </p>
+                    </div>
+                    <button class="btn-add" @click="abrirModal = true">
+                        <Plus class="icon" />
+                        Nova Atividade
+                    </button>
+                </div>
+
+                <!-- pesquisa -->
+                <div class="search">
+                    <Search class="icon" />
+                    <input placeholder="Buscar atividades..." />
+                </div>
+
+                <!-- state -->
+                <!-- <div class="empty-state">
+                    <div class="empty-icon">
+                        <FileText class="icon-large" />
+                    </div>
+                    <h3> Nenhuma atividade encontrada </h3>
+                    <p> Crie sua atividade </p>
+                    <button class="btn-create">
+                        <Plus class="icon" />
+                        Nova Atividade
+                    </button>
+                </div> -->
+            </div>
+        </div>
     </div>
+    <popUpCadastrarAtividade
+        v-if="abrirModal"
+        @close="abrirModal = false"
+        @save="(atividade)=>console.log(atividade)"
+    />
 </template>
 <style scoped src="../style/adicionarAtividade.css"></style>
